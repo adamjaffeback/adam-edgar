586a5f0808d0cf473b48ea05508c76900b498f7f
Adam Jaffe Back <adb33@email.arizona.edu>
2019-07-20 14:00:16 -0700

    (refactor) Rename fetch to extract.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const validation = require('./helpers/inputValidation');
-const fetch = require('./helpers/fetch');
+const extract = require('./helpers/extract');
 const read = require('./helpers/read');
 
 module.exports = async function processEdgarData (quarter, year) {
   validation.checkQuarter(quarter);
   validation.checkYear(year);
 
-  const fileName = await fetch.filing(quarter, year);
+  const fileName = await extract.filing(quarter, year);
   const fileData = await read.zip(fileName);
   return fileData;
 }
